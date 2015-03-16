c7fb3abb29cc7985f10373bf9b89a60a0a721452
haroldolivieri <olivierisoares@gmail.com>
2015-03-16 16:44:03 -0300

    Tab Bar
    adição de biblioteca tabBar;
    novos elementos

--- EduOrganizer/ViewController.swift
+++ EduOrganizer/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITabBarController, FancyTabBarDelegate {
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+    }
+    
+    var fancyTabBar : FancyTabBar!;
+    var backGround : UIImageView!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        var buttons: [String] = ["actionNote", "actionTeacher", "actionClass", "actionTask"];
+        var image : UIImage = UIImage(named: "mainButton")!;
         
+        fancyTabBar = FancyTabBar(frame: view.frame);
+        fancyTabBar.setUpChoices(self, choices: buttons, withMainButtonImage: image);
+        fancyTabBar.delegate = self;
+        
+        view.addSubview(fancyTabBar);
+        
+    }
+    func didExpand() {
+        if(backGround == nil){
+            backGround = UIImageView(frame: view.bounds);
+            backGround.alpha = 0;
+            view.addSubview(backGround);
+        }
+        
+        UIView.animateWithDuration(0.3, animations: {
+            self.backGround.alpha = 1;
+            }, completion: {
+                (value: Bool) in
+            }
+        );
+        
+        view.bringSubviewToFront(fancyTabBar);
+
+        var backgroundImage : UIImage = view.convertViewToImage();
+        var tintColor : UIColor = UIColor.blackColor().colorWithAlphaComponent(0.5);
+
+        
+        var image :  UIImage = backgroundImage.applyBlurWithRadius(10, tintColor: tintColor, saturationDeltaFactor: 1.8, maskImage: nil);
+        backGround.image = image;
         
     }
     
+    func didCollapse() {
+        
+        
+        UIView.animateWithDuration(0.3, animations: {
+            self.backGround.alpha = 0;
+            }, completion: {
+                (value: Bool) in
+                
+                if(value) {
+                    self.backGround.removeFromSuperview();
+                    self.backGround = nil;
+                }
+                
+            }
+        );
+        
+    }
+    
+//    func optionsButton(optionButton: UIButton!, didSelectItem index: Int32) {
+//            if (index == 1) {
+//                performSegueWithIdentifier("MY NEXT VIEW", sender: self);
+//            }
+//    }
+    
     override func viewDidAppear(animated: Bool) {
-        let feedScreen:FeedScreen=FeedScreen();
-        self.presentViewController(feedScreen, animated: true, completion: nil);
+       // let feedScreen:FeedScreen=FeedScreen();
+       // self.presentViewController(feedScreen, animated: true, completion: nil);
     }
 
     override func didReceiveMemoryWarning() {
---
+++ EduOrganizer/temp.m
@@ -0,0 +1,9 @@
+//
+//  temp.m
+//  EduOrganizer
+//
+//  Created by Haroldo Olivieri on 3/16/15.
+//  Copyright (c) 2015 Pietro Ribeiro Pepe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
