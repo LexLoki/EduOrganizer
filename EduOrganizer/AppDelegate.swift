ee11bc50ee98751bf3f6de519653f25dbb6e33e3
haroldolivieri <olivierisoares@gmail.com>
2015-03-20 17:42:07 -0300

    nao ignorem nada nessa PORRAAAA

---
+++ EduOrganizer/AppDelegate.swift
@@ -0,0 +1,103 @@
+//
+//  AppDelegate.swift
+//  EduOrganizer
+//
+//  Created by Pietro Ribeiro Pepe on 3/11/15.
+//  Copyright (c) 2015 Pietro Ribeiro Pepe. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window?.tintColor =  UIColorFromRGB(0xFFC561);
+
+        checkFiles("userData", type: ".plist");
+        
+        return true
+    }
+    
+    func checkFiles(filename:String, type:String){
+        var destPath:String = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String;
+        var imgPath:String = destPath;
+        destPath = destPath.stringByAppendingPathComponent("\(filename)\(type)");
+        // If the file doesn't exist in the Documents Folder, copy it.
+        var fileManager:NSFileManager = NSFileManager.defaultManager();
+        println(destPath);
+        if(!fileManager.fileExistsAtPath(destPath)){
+            let sourcePath:String = NSBundle.mainBundle().pathForResource(filename, ofType: type)!;
+            fileManager.copyItemAtPath(sourcePath, toPath: destPath, error: nil);
+            //let sourcePath:NSString = NSBundle.mainBundle().pathForResource(filename, type)
+        }
+        
+        imgPath = imgPath.stringByAppendingPathComponent("imgProf");
+        fileManager.createDirectoryAtPath(imgPath, withIntermediateDirectories:false, attributes:nil, error:nil);
+        
+        var filename:String = "christine";
+        var img:UIImage = UIImage(named: filename)!;
+        var pngData:NSData = UIImagePNGRepresentation(img);
+        //let placePath:String = NSBundle.mainBundle().pathForResource(filename, ofType: ".png")!;
+        var img2Path = imgPath.stringByAppendingPathComponent("\(filename).png");
+        
+        fileManager.createFileAtPath(img2Path, contents: pngData, attributes:nil);
+        
+        filename = "hugofuks";
+        img = UIImage(named: filename)!;
+        pngData = UIImagePNGRepresentation(img);
+        //let placePath:String = NSBundle.mainBundle().pathForResource(filename, ofType: ".png")!;
+        img2Path = imgPath.stringByAppendingPathComponent("\(filename).png");
+        
+        fileManager.createFileAtPath(img2Path, contents: pngData, attributes:nil);
+        
+        filename = "spesco";
+        img = UIImage(named: filename)!;
+        pngData = UIImagePNGRepresentation(img);
+        //let placePath:String = NSBundle.mainBundle().pathForResource(filename, ofType: ".png")!;
+        img2Path = imgPath.stringByAppendingPathComponent("\(filename).png");
+        
+        fileManager.createFileAtPath(img2Path, contents: pngData, attributes:nil);
+        
+    }
+
+    
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
