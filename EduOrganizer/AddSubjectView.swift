37cb010a67dc194fa0abc926b1715c90f61f18bc
juliana dana <juliana_zilber@hotmail.com>
2015-04-05 14:25:53 -0300

    classe AddSubjectView
    comecei a fazer essa classe

---
+++ EduOrganizer/AddSubjectView.swift
@@ -0,0 +1,33 @@
+//
+//  AddSubjectView.swift
+//  Stud
+//
+//  Created by Juliana Zilberberg on 4/5/15.
+//  Copyright (c) 2015 Pietro Ribeiro Pepe. All rights reserved.
+//
+
+import Foundation
+
+class AddSubjectView : UIView{
+    
+    
+    var nameText : UITextField!;
+    var code : UITextField!;
+    var startDate : UITextField!;
+    var endDate : UITextField!;
+    var schedule : UITextField!;
+    var professor : UITextField!;
+    
+    init(view: UIView, parent: UIViewController) {
+        super.init(frame: view.frame);
+
+        
+        
+        
+    }
+    
+        required init(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")}
+
+
+}
