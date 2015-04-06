b11580e6c3e4e4f15d48eee4ea42319f26b4e61d
Gustavo Severo <giogusbr@gmail.com>
2015-04-05 21:33:57 -0300

    AddProfessorView.swift design changes
    All changes with the guidelines done.

--- EduOrganizer/AddProfessorView.swift
+++ EduOrganizer/AddProfessorView.swift
@@ -16,64 +16,93 @@ class AddProfessorView : UIView{
     var cancelButton:UIButton!;
     var saveButton:UIButton!;
     var addSubject:UITextField!;
+    var newLabel: UILabel!;
     
     init(view: UIView, parent: UIViewController) {
         super.init(frame: view.frame);
         
         let altura:CGFloat =  view.frame.height*0.08;
+        let alturaLabel:CGFloat = view.frame.height*0.09
+        
+        // Blur Effect
+        var blur = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+        var blurView = UIVisualEffectView(effect: blur)
+        blurView.frame = view.bounds
+        blurView.alpha = 0.9
         
         //declaracao da area e definicao do tamanho//
-        var area:CGRect = CGRectMake(view.frame.width*0.075, view.frame.height*0.2, view.frame.width*0.85
+        var area:CGRect = CGRectMake(view.frame.width*0.05, view.frame.height*0.2, view.frame.width*0.90
             , altura*5);
+        
         //definindo que eh a view eh a tela//
         var tela : UIView = UIView(frame: area)
         //acessando uma propriedade da tela(UIView) alterando as bordas//
-        tela.layer.cornerRadius = 7
-        tela.backgroundColor = UIColor.whiteColor()
+        tela.layer.cornerRadius = 5
+        tela.backgroundColor = UIColor.UIColorFromRGB(0xD5D9DF)
         
         //adicionando uma subview(tela) dentro da tela principal//
+        view.addSubview(blurView)
         view.addSubview(tela)
         
         //criando campos de texto//
         let spacing:CGFloat = area.width*0.05;
+        let spacingLabel: CGFloat = (blurView.frame).width*0.19
+        
+        newLabel = UILabel(frame: CGRectMake(spacingLabel, alturaLabel, area.width*0.7, alturaLabel/2))
+        newLabel.textAlignment = NSTextAlignment.Center
+        newLabel.text = "New Professor"
+        newLabel.textColor = UIColor.whiteColor()
+        newLabel.font = UIFont(name: "AvenirNext-Regular", size: 28)
+        
         //botao pra cancelar
-        cancelButton = UIButton(frame: CGRectMake(spacing, 0, area.width*0.3, altura))
-        cancelButton.backgroundColor = UIColor.whiteColor();
+        cancelButton = UIButton(frame: CGRectMake(spacing, altura/4, area.width*0.22, altura/2))
         cancelButton.setTitle("Cancel", forState: UIControlState.Normal)
-        cancelButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        cancelButton.titleLabel!.font = UIFont(name: "AvenirNext-Regular", size: 20)
+        cancelButton.setTitleColor(UIColor.UIColorFromRGB(0x1A242E), forState: UIControlState.Normal)
         
         //botao pra salver
-        saveButton = UIButton(frame: CGRectMake(area.width - spacing - area.width*0.3, 0, area.width*0.3, altura))
-        saveButton.backgroundColor = UIColor.whiteColor();
+        saveButton = UIButton(frame: CGRectMake(area.width - spacing - area.width*0.18, altura/4, area.width*0.2, altura/2))
         saveButton.setTitle("Save", forState: UIControlState.Normal)
-        saveButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        saveButton.titleLabel!.font = UIFont(name: "AvenirNext-Medium", size: 20)
+        saveButton.setTitleColor(UIColor.UIColorFromRGB(0x1A242E), forState: UIControlState.Normal)
         
-        nomeText = UITextField(frame: CGRectMake(spacing, cancelButton.frame.origin.y + altura, area.width, altura))
+        // TEXT FIELDS
+        
+        // Name Text Field
+        
+        nomeText = UITextField(frame: CGRectMake(spacing, cancelButton.frame.origin.y + altura/1.4, area.width, altura))
+        nomeText.font = UIFont(name: "AvenirNext-Regular", size: 18)
         nomeText.autocorrectionType = UITextAutocorrectionType.No;
         nomeText.placeholder = "Name"
         
         emailText = UITextField(frame: CGRectMake(spacing, nomeText.frame.origin.y + altura, area.width, altura))
+        emailText.font = UIFont(name: "AvenirNext-Regular", size: 18)
         emailText.autocorrectionType = UITextAutocorrectionType.No;
-        emailText.placeholder = "e-mail"
+        emailText.placeholder = "Email"
         
         subjectCode = UITextField(frame: CGRectMake(spacing, emailText.frame.origin.y + altura, area.width, altura))
-        subjectCode.placeholder = "subject code"
+        subjectCode.font = UIFont(name: "AvenirNext-Regular", size: 18)
+        subjectCode.placeholder = "Subject code"
         subjectCode.inputView = UIPickerView()
         
         addSubject = UITextField(frame: CGRectMake(spacing, subjectCode.frame.origin.y + altura, area.width, altura))
+        addSubject.font = UIFont(name: "AvenirNext-Regular", size: 18)
         addSubject.placeholder = "Add more subjects"
         
-        var lineView : UIView = UIView (frame: CGRectMake(0, cancelButton.frame.origin.y + altura/1, area.width, 1))
-        lineView.backgroundColor = UIColor.blackColor()
+        var lineView : UIView = UIView (frame: CGRectMake(0, cancelButton.frame.origin.y + altura/1.4, area.width, 1))
+        lineView.backgroundColor = UIColor.UIColorFromRGB(0x979797).colorWithAlphaComponent(0.7);
         
         var lineView1 : UIView = UIView (frame: CGRectMake(0, nomeText.frame.origin.y + altura/1, area.width, 1))
-        lineView1.backgroundColor = UIColor.blackColor()
+        lineView1.backgroundColor = UIColor.UIColorFromRGB(0x979797).colorWithAlphaComponent(0.7);
+        
         var lineView2 : UIView = UIView (frame: CGRectMake(0, emailText.frame.origin.y + altura/1, area.width, 1))
-        lineView2.backgroundColor = UIColor.blackColor()
+        lineView2.backgroundColor = UIColor.UIColorFromRGB(0x979797).colorWithAlphaComponent(0.7);
+        
         var lineView3 : UIView = UIView (frame: CGRectMake(0, subjectCode.frame.origin.y + altura/1, area.width, 1))
-        lineView3.backgroundColor = UIColor.blackColor()
+        lineView3.backgroundColor = UIColor.UIColorFromRGB(0x979797).colorWithAlphaComponent(0.7);
         
         //adicionar a view que criei na view pra aparecer na tela principal//
+        blurView.addSubview(newLabel)
         tela.addSubview(nomeText)
         tela.addSubview(emailText)
         tela.addSubview(subjectCode)
