d3708c8b895dd95105c75161b0419d940d434401
LexLoki <pietroribeiropepe@hotmail.com>
2015-04-07 11:37:49 -0300

    Fix extension

---
+++ EduOrganizer/AddSubjectVC.swift
@@ -0,0 +1,18 @@
+//
+//  AddSubjectVC.swift
+//  Stud
+//
+//  Created by Pietro Ribeiro Pepe on 4/7/15.
+//  Copyright (c) 2015 Pietro Ribeiro Pepe. All rights reserved.
+//
+
+import Foundation
+
+
+class AddSubjectVC{
+    
+    
+    //Left to implement
+    
+    
+}
